47cd0c60bd498f70894c6362a8a37ddfe9f8bff0
sharkspirit <Antonio.m.pinto@ist.utl.pt>
2016-04-01 03:50:10 +0100

    git didnt detect changes to this one so, here it is

--- UbiBikeMobile/app/src/main/java/cmu/tecnico/ubibikemobile/RegisterActivity.java
+++ UbiBikeMobile/app/src/main/java/cmu/tecnico/ubibikemobile/RegisterActivity.java
@@ -1,13 +1,18 @@
 package cmu.tecnico.ubibikemobile;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    Intent myIntent;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +20,14 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        button = (Button) findViewById(R.id.register);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                myIntent = new Intent(RegisterActivity.this, MainActivity.class);
+                RegisterActivity.this.startActivity(myIntent);
+            }
+        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
