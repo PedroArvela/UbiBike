e343b246bca3e035120157f5c8772fbc8c7e26ab
Pedro Arvela <jp.arvela@gmail.com>
2016-04-07 12:05:00 +0100

    Remove FAB. Add initial code for back nav
    https://developer.android.com/training/implementing-navigation/temporal.html

--- UbiBikeMobile/app/src/main/java/cmu/tecnico/ubibikemobile/CyclistsList.java
+++ UbiBikeMobile/app/src/main/java/cmu/tecnico/ubibikemobile/CyclistsList.java
@@ -43,14 +43,14 @@ public class CyclistsList extends AppCompatActivity {
 
         listView.setAdapter(adapter);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
     }
 
 
