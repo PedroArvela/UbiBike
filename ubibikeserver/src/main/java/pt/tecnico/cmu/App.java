c9f8c4a1a56d5a9fef11ceeebdf5775f0ca28c6f
Pedro Arvela <jp.arvela@gmail.com>
2016-04-23 19:11:49 +0100

    Add path and query handling functions

--- ubibikeserver/src/main/java/pt/tecnico/cmu/App.java
+++ ubibikeserver/src/main/java/pt/tecnico/cmu/App.java
@@ -3,6 +3,10 @@ package pt.tecnico.cmu;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.Executors;
 
 import com.sun.net.httpserver.HttpExchange;
@@ -23,14 +27,55 @@ public class App {
 
 		@Override
 		public void handle(HttpExchange t) throws IOException {
-			String query = t.getRequestURI().getQuery();
-			// TODO: process response according to requested query
-			String response = "Response";
+			String pathString = t.getRequestURI().getPath();
+			String queryString = t.getRequestURI().getQuery();
 
-            t.sendResponseHeaders(200, response.length());
-            OutputStream os = t.getResponseBody();
-            os.write(response.getBytes());
-            os.close();
+			List<String> path = processPath(pathString);
+			Map<String, String> queries = processQueries(queryString);
+
+			String response = "";
+
+			t.sendResponseHeaders(200, response.length());
+			OutputStream os = t.getResponseBody();
+			os.write(response.getBytes());
+			os.close();
+		}
+
+		public static List<String> processPath(String pathString) {
+			List<String> components = new ArrayList<String>();
+
+			if (pathString == null) {
+				pathString = "";
+			}
+
+			for (String component : pathString.split("/")) {
+				if (!component.isEmpty()) {
+					components.add(component);
+				}
+			}
+
+			return components;
+		}
+
+		public static Map<String, String> processQueries(String queryString) {
+			Map<String, String> queries = new HashMap<String, String>();
+
+			if (queryString == null) {
+				queryString = "";
+			}
+
+			for (String query : queryString.split("&")) {
+				if (!query.isEmpty()) {
+					String[] queryArgs = query.split("=", 2);
+					if (queryArgs.length == 2) {
+						queries.put(queryArgs[0], queryArgs[1]);
+					} else {
+						queries.put(queryArgs[0], "true");
+					}
+				}
+			}
+
+			return queries;
 		}
 
 	}
