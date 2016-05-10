048f90ff921deaa4c96ec8e9524bc97a0eb8a503
Pedro Arvela <jp.arvela@gmail.com>
2016-05-10 17:15:14 +0100

    Make username and password verification
    Very unsafe at the moment

--- UbiBikeMobile/app/src/main/java/cmu/tecnico/ubibikemobile/RegisterActivity.java
+++ UbiBikeMobile/app/src/main/java/cmu/tecnico/ubibikemobile/RegisterActivity.java
@@ -2,31 +2,20 @@ package cmu.tecnico.ubibikemobile;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
-import android.os.Messenger;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.util.Pair;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import java.io.Console;
-import java.net.MalformedURLException;
-import java.net.URL;
-
 import cmu.tecnico.ubibikemobile.asyncTasks.LoginTask;
-import cmu.tecnico.ubibikemobile.services.NetworkConnectionService;
 
 public class RegisterActivity extends AppCompatActivity {
 
     Intent myIntent;
     Button button;
+    EditText txtUsername;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,18 +24,15 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         final EditText txtUsername = (EditText) findViewById(R.id.txt_username);
+        final EditText txtPassword = (EditText) findViewById(R.id.txt_password);
         setSupportActionBar(toolbar);
 
         button = (Button) findViewById(R.id.btn_register);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                try {
-                    new LoginTask().execute(
-                            new Pair<RegisterActivity, URL>(RegisterActivity.this,
-                                    new URL("http", "web.tecnico.ulisboa.pt", "/~jose.pedro.arvela/ubibike/user/" + txtUsername.getText().toString())));
-                } catch (MalformedURLException e) {
-                    // ignore
-                }
+                new LoginTask(RegisterActivity.this,
+                        txtUsername.getText().toString(),
+                        txtPassword.getText().toString()).execute(true);
             }
         });
     }
--- UbiBikeMobile/app/src/main/java/cmu/tecnico/ubibikemobile/asyncTasks/LoginTask.java
+++ UbiBikeMobile/app/src/main/java/cmu/tecnico/ubibikemobile/asyncTasks/LoginTask.java
@@ -2,6 +2,7 @@ package cmu.tecnico.ubibikemobile.asyncTasks;
 
 import android.os.AsyncTask;
 import android.util.Pair;
+import android.widget.EditText;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
@@ -11,40 +12,53 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.List;
 
+import cmu.tecnico.ubibikemobile.R;
 import cmu.tecnico.ubibikemobile.RegisterActivity;
 
-public class LoginTask extends AsyncTask<Pair<RegisterActivity, URL>, Boolean, List<String>> {
+public class LoginTask extends AsyncTask<Boolean, Boolean, Pair<String, Boolean>> {
     RegisterActivity activity;
+    String username;
+    String password;
+
+    public LoginTask(RegisterActivity activity, String username, String password) {
+        this.activity = activity;
+        this.username = username;
+        this.password = password;
+    }
 
     @Override
-    protected List<String> doInBackground(Pair<RegisterActivity, URL>... params) {
-        List<String> result = new ArrayList<String>();
-        activity = params[0].first;
+    protected Pair<String, Boolean> doInBackground(Boolean... params) {
+        Pair<String, Boolean> result = new Pair<String, Boolean>(username, false);
 
         try {
-            HttpURLConnection conn = (HttpURLConnection) params[0].second.openConnection();
+            URL url = new URL(activity.getResources().getString(R.string.protocol),
+                    activity.getResources().getString(R.string.domain),
+                    activity.getResources().getInteger(R.integer.port),
+                    activity.getResources().getString(R.string.base) + "user/" + username + "/password");
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             InputStream in = new BufferedInputStream(conn.getInputStream());
             BufferedReader rd = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
 
-            String line = "";
-            while ((line = rd.readLine()) != null) {
-                result.add(line);
-            }
+            int response = conn.getResponseCode();
 
+            if(response == 200) {
+                result = new Pair<String, Boolean>(username, password.equals(rd.readLine()));
+            }
         } catch (IOException e) {
-            return null;
+            // nothing to do
         } finally {
             return result;
         }
     }
 
     @Override
-    protected void onPostExecute(List<String> result) {
-        if (result.size() >= 1) {
-            activity.login(result.get(0));
+    protected void onPostExecute(Pair<String, Boolean> result) {
+        if (result.second) {
+            activity.login(result.first);
+        } else {
+            ((EditText) activity.findViewById(R.id.txt_username)).setError("Username or password wrong");
         }
     }
 }
