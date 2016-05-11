676817e4def1821206c970cb4aba2df3c7173f5a
Pedro Arvela <jp.arvela@gmail.com>
2016-05-11 13:52:07 +0100

    Server initial commit

---
+++ ubibikeserver/src/main/java/cmu/tecnico/App.java
@@ -0,0 +1,13 @@
+package cmu.tecnico;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
