12ada3ec676a05c08db8cf3adc6df7369a5e3832
sharkspirit <Antonio.m.pinto@ist.utl.pt>
2016-05-13 02:05:02 +0100

    Server Interaction tasks

--- UbiBikeMobile/app/src/main/java/cmu/tecnico/ubibikemobile/asyncTasks/BikeInteractionTask.java
+++ UbiBikeMobile/app/src/main/java/cmu/tecnico/ubibikemobile/asyncTasks/BikeInteractionTask.java
@@ -1,25 +1,23 @@
 package cmu.tecnico.ubibikemobile.asyncTasks;
 
 import android.content.res.Resources;
-import android.location.Geocoder;
 import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
 import android.util.Pair;
 
-import com.google.android.gms.maps.model.LatLng;
-
 import java.net.MalformedURLException;
-import java.util.ArrayList;
 import java.util.List;
 
 import cmu.tecnico.ubibikemobile.App;
 import cmu.tecnico.ubibikemobile.R;
 import cmu.tecnico.ubibikemobile.helpers.URLHelper;
-import cmu.tecnico.ubibikemobile.models.Trajectory;
 
-public class TrajectorySendTask extends AsyncTask<String, Boolean, Boolean> {
+/**
+ * Created by Toninho on 13/05/2016.
+ */
+public class BikeInteractionTask extends AsyncTask<String, Boolean, Boolean> {
     App app;
     Handler handler;
     Resources resources;
@@ -27,7 +25,7 @@ public class TrajectorySendTask extends AsyncTask<String, Boolean, Boolean> {
 
     int responseCode = 0;
 
-    public TrajectorySendTask(App app, Handler handler, Resources resources, String username) {
+    public BikeInteractionTask(App app, Handler handler, Resources resources, String username) {
         this.app = app;
         this.handler = handler;
         this.resources = resources;
@@ -37,11 +35,10 @@ public class TrajectorySendTask extends AsyncTask<String, Boolean, Boolean> {
     @Override
     protected Boolean doInBackground(String... params) {
         URLHelper url = new URLHelper(resources);
-
         Boolean result = true;
-
-        //trajectories are a JSON'ed array of "lat,long" positions
-        String query = "newTrajectory?user=" + username + "&trajectories=" + params[0];
+        //Action either "PICKUP" or "DROPOFF"
+        //station ... some kind of id depending on what the server provices to the app to identify the stations
+        String query = "bikeInteraction?user=" + username + "&action=" + params[0] + "&station="+ params[1];
         try {
             Pair<Integer, List<String>> response = url.fetchUrl(query);
 
@@ -52,7 +49,7 @@ public class TrajectorySendTask extends AsyncTask<String, Boolean, Boolean> {
 
         } catch (MalformedURLException e) {
             Log.e("TrajectoryTask", "URL was malformed:\t" + resources.getString(R.string.base) +
-                    "newTrajectory?user=" + username + "&trajectories=" + params[0]);
+                    "bikeInteraction?user=" + username + "&action=" + params[0] + "&station="+ params[1]);
         } finally {
             return result;
         }
